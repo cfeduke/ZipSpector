2099d68f19e82965da6d03b765c3bffafc144a36
Charles Feduke <charles.feduke@gmail.com>
2011-03-01 07:30:55 -0500

    initial commit

---
+++ MyDocument.h
@@ -0,0 +1,15 @@
+//
+//  MyDocument.h
+//  ZipSpector
+//
+//  Created by Charles Feduke on 3/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyDocument : NSDocument
+{
+}
+@end
---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ZipSpector
+//
+//  Created by Charles Feduke on 3/1/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
